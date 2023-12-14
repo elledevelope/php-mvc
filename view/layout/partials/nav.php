09d1e8ab3ef7840bc145d878e165e65dcff1d8b0
Elmira PETIT <elledevelope@gmail.com>
2023-12-14 12:28:04 +0100

    New style for nav

--- view/layout/partials/nav.php
+++ view/layout/partials/nav.php
@@ -1,10 +1,30 @@
-<nav>
-    <ul>
-        <li><a href="<?= $view->path('home'); ?>">Home</a></li>
-        <li><a href="<?= $view->path('articles'); ?>">Articles</a></li>
-        <li><a href="<?= $view->path('contact'); ?>">Contact</a></li>
-        <li><a href="<?= $view->path('about'); ?>">About</a></li>
-        <li><a href="<?= $view->path('single', array(12)); ?>">Single</a></li>
-        <li><a href="<?= $view->path('single2', array(12, 'dedede')); ?>">Single2</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">myApp</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="<?= $view->path('home'); ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $view->path('articles'); ?>">Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $view->path('contact'); ?>">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $view->path('about'); ?>">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $view->path('single', array(12)); ?>">Single</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $view->path('single2', array(12, 'dedede')); ?>">Single2</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
