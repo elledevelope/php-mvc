78c70ef31aff7b61acad602f372e4ab63d996f1b
Elmira PETIT <elledevelope@gmail.com>
2023-12-15 11:07:19 +0100

    Update the way to use Model in PostModel

--- app/Model/PostModel.php
+++ app/Model/PostModel.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Model;
 
-class PostModel extends \App\Weblitzer\Model 
+use \App\Weblitzer\Model;
+
+class PostModel extends Model
+// or in one line: class PostModel extends \App\Weblitzer\Model 
 {
     // protected static $table = 'post';
     protected static $table = 'note';
-
-	
 }
