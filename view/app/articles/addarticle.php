9fb9d153f9e2de2f83f123f911bac6fb45b64283
Elmira PETIT <elledevelope@gmail.com>
2023-12-15 14:40:48 +0100

    Added style to addarticle.php

--- view/app/articles/addarticle.php
+++ view/app/articles/addarticle.php
@@ -2,20 +2,25 @@
 // $this->dd($formAdd);
 ?>
 
-<section>
-    <h2>Add Article</h2>
+<section class="container-md">
+    <h2 class="mb-3">Add Article</h2>
 
     <form action="" method="post">
         <div class="mb-3">
-            <?= $formAdd->label('titre', ['class' => 'form-label']) ?>
+            <?= $formAdd->label('titre') ?>
             <?= $formAdd->input('titre') ?>
-            <?= $formAdd->error('titre') ?> <br>
+            <?= $formAdd->error('titre') ?>
+        </div>
+        <br>
 
+        <div class="mb-3">
             <?= $formAdd->label('content') ?>
             <?= $formAdd->textarea('content') ?>
-            <?= $formAdd->error('content') ?> <br>
-
-            <?= $formAdd->submit('submitted', $value = 'Send',  ['class' => 'btn btn-primary']) ?>
+            <?= $formAdd->error('content') ?>
         </div>
+        <br>
+
+        <?= $formAdd->submit('submitted', $value = 'Send') ?>
+
     </form>
 </section>
