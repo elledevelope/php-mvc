d67633a0eed62e89749117bbe26a45bc4332584c
Elmira PETIT <elledevelope@gmail.com>
2023-12-15 11:48:23 +0100

    Added form

--- view/app/articles/addarticle.php
+++ view/app/articles/addarticle.php
@@ -1,3 +1,21 @@
 <?php
+// $this->dd($formAdd);
+?>
 
-$this->dd($formAdd);
+<section>
+    <h2>Add Article</h2>
+
+    <form action="" method="post">
+        <div class="mb-3">
+            <?= $formAdd->label('titre', ['class' => 'form-label']) ?>
+            <?= $formAdd->input('titre') ?>
+            <?= $formAdd->error('titre') ?> <br>
+
+            <?= $formAdd->label('content') ?>
+            <?= $formAdd->textarea('content') ?>
+            <?= $formAdd->error('content') ?> <br>
+
+            <?= $formAdd->submit('submit', $value = 'Send',  ['class' => 'btn btn-primary']) ?>
+        </div>
+    </form>
+</section>
